329541f1ad467da049b2bc2cfa1782ec3a92ef82
Chunhua (Leo) Liao <liaoch@llnl.gov>
2013-01-24 14:42:49 -0800

    (OMP ACC) added one more test case * skip SageInterface::clearUnusedVariableSymbols() in AST consistency  test since #define N 100 will cause a fake symbol for N be created with  unknowntype * SageBuilder: set parent for the fake symbol

--- src/frontend/SageIII/sageInterface/sageBuilder.C
+++ src/frontend/SageIII/sageInterface/sageBuilder.C
@@ -2656,6 +2656,7 @@ SageBuilder::buildVarRefExp(const SgName& name, SgScopeStatement* scope/*=NULL*/
           SgInitializedName * name1 = buildInitializedName(name,SgTypeUnknown::createType());
           name1->set_scope(scope); //buildInitializedName() does not set scope for various reasons
           varSymbol= new SgVariableSymbol(name1);
+          varSymbol->set_parent(scope);
         }
      ROSE_ASSERT(varSymbol); 
 
